f2ec959eb8e121119010e9319caeb0457de45f43
Mark Tai <taifighterm@gmail.com>
2015-12-13 17:46:16 -0800

    parse toml as html inside <p>

--- pages/posts/PostsCtl.js
+++ pages/posts/PostsCtl.js
@@ -1,4 +1,4 @@
-myapp.controller("PostsCtl", ["$scope", "$rootScope", "$http", "$location", function($scope, $rootScope, $http, $location) {
+myapp.controller("PostsCtl", ["$scope", "$rootScope", "$http", "$location", "$sce",  function($scope, $rootScope, $http, $location, $sce) {
 	$rootScope.page = "posts";
 
 	$scope.post = $location.hash()
@@ -8,8 +8,14 @@ myapp.controller("PostsCtl", ["$scope", "$rootScope", "$http", "$location", func
 	var getPost = function(post) {
 		$http.get('/api/posts/' + post).then(function(result){ 
 			$scope.postData = result.data["Post"]
+            for (var par in $scope.postData.Content) {
+                $scope.pars.append($sce.trustAsHtml(par))
+            }
+
 		})
 	}
+
+    $scope.pars = []
 
 	$scope.generateTag = function() {
 		var retStr = "Posted"
