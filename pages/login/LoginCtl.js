b106522ec14f11032833d1644acea30ea75c287c
marktai <taifighterm@gmail.com>
2016-01-07 15:16:11 -0800

    working authed moves

--- pages/login/LoginCtl.js
+++ pages/login/LoginCtl.js
@@ -35,6 +35,48 @@ myapp.controller("LoginCtl", ["$scope", "$rootScope", "$http", "$location", "$sc
 		});
 	}
 
+	var getGame = function(gameID) {
+		$http.get('/T9/games/' + gameID + '/string').then(function(result){ 
+			$scope.board = ""
+			var boardArray = result.data["Board"]
+            for (line of boardArray) {
+                $scope.board += line + "\n"
+            }
+		})
+		$http.get('/T9/games/' + gameID).then(function(result){ 
+			$scope.gameData = result.data["Game"]
+    		$scope.players = $scope.gameData["Players"]
+    		$scope.player = $scope.players[Math.floor($scope.gameData["Turn"]/10)] + ""
+    		if ($scope.gameData["Turn"]%10 < 9) {
+	    		$scope.box = $scope.gameData["Turn"]%10 + ""
+    		}
+		})
+	}
+
+    var makeMove = function(game, player, box, square) {
+
+    	var time = Math.floor(Date.now() / 1000);
+		var urlWithoutT9 = '/games/' + game + '?Player=' + player + '&Box=' + box + '&Square=' + square;
+    	var message = time + ":" + urlWithoutT9;
+		var hash = CryptoJS.HmacSHA256(message, $scope.secret);
+		// var hashInBase64 = CryptoJS.enc.Base64.stringify(hash);
+
+
+		var url = '/T9' + urlWithoutT9;
+		var data = '';
+		var config = {
+			'headers': {
+				'HMAC' : hash,
+				'Encoding' : 'hex',
+				'Time-Sent' : time,
+			}
+		}
+        $http.post(url, data, config).then(function(result){
+                $scope.getGame($scope.game)
+        }, function(error) {
+                $scope.error = error.data["Error"]
+        })
+    }
 
 	$scope.login = function() {
 		login($scope.usr, $scope.pwd);
@@ -44,4 +86,33 @@ myapp.controller("LoginCtl", ["$scope", "$rootScope", "$http", "$location", "$sc
 	$scope.verifySecret = function() {
 		verifySecret($scope.usr, $scope.secret);
 	}
+
+
+	$scope.game = $location.hash()
+
+	$scope.gameData = {}
+    $scope.board = ""
+
+    $scope.players = []
+    $scope.boxes = []
+    $scope.squares = []
+
+    for (var i = 0; i < 9; i++) {
+    	$scope.boxes.push('' + i)
+    	$scope.squares.push('' + i)
+    }
+
+    $scope.player = ''
+    $scope.box = $scope.boxes[0]
+    $scope.square = $scope.squares[0]
+
+	$scope.getGame = function() {
+		getGame($scope.game);
+	}
+
+    $scope.makeMove = function(game, player, box, square) {
+    	makeMove($scope.game, $scope.player, $scope.box, $scope.square);
+    }
+
+	$scope.getGame();
 }])
