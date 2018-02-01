711481692afbf6e1b2ba16e84e6490b796dbc63e
Mark <mark@marktai.com>
2018-02-01 01:19:45 +0000

    bomb page improvements

--- pages/bomb/BombCtl.js
+++ pages/bomb/BombCtl.js
@@ -2,7 +2,9 @@ marktai.controller("BombCtl", ["$scope", "$rootScope", "$route", "$sce", "$windo
 	$rootScope.info = ""
 	$rootScope.error = ""
 	$rootScope.page = "bombmanual";
-	var url = "https://www.marktai.com/download/me/Bomb-Defusal-Manual_1.pdf";
+	var url = "https://www.marktai.com/download/me/Bomb-Defusal-Manual_1_stripped.pdf";
 	$scope.google_url = $sce.trustAsResourceUrl("https://docs.google.com/viewer?url=" + url);
 	$scope.url = $sce.trustAsResourceUrl(url);
+	var pages = ["Simple Wires", "Colored Button", "Symbol Keypads", "Simon Says", "Word Display/Buttons", "Number Display/Buttons (Memory)", "Morse Code", "Complicated Wires", "Wire Sequences", "Mazes", "Passwords", "Venting Gas", "Capacitor", "Knobs", "Appendix"]
+	$scope.text = "Index: \n" + pages.map(function(s){return '    '+s}).join('\n');
 }])
